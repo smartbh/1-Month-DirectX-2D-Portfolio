ee1d8dcadf669817c21ff647d0e03c0d9589cb4a
smartbh <smartbh@naver.com>
2022-09-16 19:45:06 +0900

    boss room

--- Game1/SceneBossRoom.cpp
+++ Game1/SceneBossRoom.cpp
@@ -2,6 +2,20 @@
 
 SceneBossRoom::SceneBossRoom()
 {
+    bg = new ObRect();
+    bg->scale = Vector2(10000.0f, 10000.0f);
+    bg->color = Color(0.0f, 0.0f, 0.0f, 1.0f);
+    bg->isFilled = true;
+
+    map = new ObTileMap();
+    map->file = "leftMap.txt";
+    map->Load();
+    map->CreateTileCost();
+    map->SetWorldPos(Vector2());
+
+    doorCol = new ObRect();
+    doorImage = new ObImage(L"");
+
 }
 
 SceneBossRoom::~SceneBossRoom()
